3181c6deddadc24928d884c3fe98458d4f721d11
Kagami <duchien230904@gmail.com>
2022-11-19 16:16:20 +0700

    Clock JavaScripts was added Meridiem feature

--- assets/JS/scripts.js
+++ assets/JS/scripts.js
@@ -24,6 +24,23 @@ const clockDigital = document.querySelector(".clock-digital");
 const hourDigital = clockDigital.querySelector(".hour-digital");
 const minuteDigital = clockDigital.querySelector(".minute-digital");
 const secondDigital = clockDigital.querySelector(".second-digital");
+const meridiemDigital = clockDigital.querySelector(".meridiem-digital");
+
+function timeModifier(element, time) {
+    if (element >= 0 && element <= 9) {
+        element = "0" + time;
+    } else {
+        element = time;
+    }
+}
+
+function meridiemModifier(element) {
+    if (element >= 0 && element <= 12) {
+        meridiemDigital.innerText = "AM";
+    } else {
+        meridiemDigital.innerText = "PM";
+    }
+}
 
 function setDigitalClock() {
     const currentTime = new Date();
@@ -31,8 +48,16 @@ function setDigitalClock() {
     const minute = currentTime.getMinutes();
     const second = currentTime.getSeconds();
     hourDigital.innerText = hour;
+    // if (hourDigital.innerText >= 0 && hourDigital.innerText <= 12) {
+    //     meridiemDigital.innerText = "AM";
+    // } else {
+    //     meridiemDigital.innerText = "PM";
+    // }
+    meridiemModifier(hourDigital.innerText);
     minuteDigital.innerText = minute;
+    timeModifier(minuteDigital.innerText, minute);
     secondDigital.innerText = second;
+    timeModifier(secondDigital.innerText, minute);
 }
 
 setDigitalClock();
